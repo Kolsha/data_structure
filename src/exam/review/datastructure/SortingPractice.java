a9fea6a599163bc0159fb52b7330793f897b6211
shanwu <shanwustore@gmail.com>
2016-11-26 20:03:24 +0800

    change: add solution to sorting practice2~ sorting anagrams

--- src/exam/review/datastructure/SortingPractice.java
+++ src/exam/review/datastructure/SortingPractice.java
@@ -8,9 +8,10 @@ import java.util.Comparator;
  */
 public class SortingPractice {
     /**
-     *  My solution to question:
-     *  "You are given two sorted arrays, A and B, and A has a large enough buffer at the end
-     *  to hold B. Write a method to merge B into A in sorted order."
+     * My solution to question:
+     * "You are given two sorted arrays, A and B, and A has a large enough buffer at the end
+     * to hold B. Write a method to merge B into A in sorted order."
+     *
      * @param bigArray
      * @param smallArray
      * @param aEndIndex
@@ -45,7 +46,33 @@ public class SortingPractice {
         }
     }
 
+    /**
+     * How to sort an array of strings with anagrams next to each other
+     * http://stackoverflow.com/questions/15515324/how-to-sort-an-array-of-strings-with-anagrams-next-to-each-other
+     *
+     * @param stringArray
+     */
+    public static void sortStringWithAnagrams(String[] stringArray) {
+        Arrays.sort(stringArray, new AnagramComparator());
+    }
+
+    public static class AnagramComparator implements Comparator<String> {
+
+        public String getSortedString(String s) {
+            char[] content = s.toCharArray();
+            Arrays.sort(content);
+            return new String(content);
+        }
+
+        public int compare(String s1, String s2) {
+            return getSortedString(s1).compareTo(getSortedString(s2));
+        }
+
+    }
+
+
     public static void main(String[] args) {
+        // Question 1 test case
         Integer[] test1 = new Integer[6];
         test1[0] = 5;
         test1[1] = 6;
@@ -63,5 +90,13 @@ public class SortingPractice {
             }
         });
         System.out.println(Arrays.toString(test1));
+
+        // Question 2 test case
+        String[] testStringArray = new String[]{"god", "dog", "abc", "cab", "man"};
+        sortStringWithAnagrams(testStringArray);
+        System.out.println(Arrays.toString(testStringArray));
+
+
+
     }
 }
