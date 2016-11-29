9348bab4d52cc829d056d1697d4d7e9b407ef4c0
shanwu <shanwustore@gmail.com>
2016-11-29 16:06:57 +0800

    change: add recursive example ~ fibonacci function

--- src/exam/review/datastructure/Recursive.java
+++ src/exam/review/datastructure/Recursive.java
@@ -10,9 +10,22 @@ public class Recursive {
         return n * getFactorial(n - 1);
     }
 
+    public static int getNthFibnacci(int n) {
+        if(n == 0) {
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        } else {
+            return getNthFibnacci(n-1) + getNthFibnacci(n-2);
+        }
+    }
+
     public static void main(String[] args) {
         int target = 5;
         System.out.println("factorial: "+target+"! = "+getFactorial(target));
+
+        target = 10;
+        System.out.println("Fibonacci: "+target+"th number = "+getNthFibnacci(target));
 
     }
 }
