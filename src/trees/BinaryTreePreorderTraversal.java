c4a1bc37a15d52131142343512297743379e955b
shanwu <shanwustore@gmail.com>
2016-12-18 10:36:23 +0800

    change: add leetcode practice~ binary tree preorder traversal

---
+++ src/trees/BinaryTreePreorderTraversal.java
@@ -0,0 +1,46 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shanwu on 16-12-18.
+ */
+public class BinaryTreePreorderTraversal {
+    /**
+     * Given a binary tree, return the preorder traversal of its nodes' values.
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList();
+
+        if (root == null) {
+            return list;
+        }
+
+
+        list.add(root.val);
+        if (root.left != null) {
+            list.addAll(preorderTraversal(root.left));
+        }
+
+        if (root.right != null) {
+            list.addAll(preorderTraversal(root.right));
+        }
+        return list;
+    }
+
+    // TODO: 16-12-18 Note: Recursive solution is trivial, could you do it iteratively?
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
