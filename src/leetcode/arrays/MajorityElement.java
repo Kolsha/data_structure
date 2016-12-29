7a6906b93b7c933961aa7f0e295157486632af5b
shanwu <shanwustore@gmail.com>
2016-12-29 19:07:26 +0800

    change: add leetcode practice~ majority element

---
+++ src/leetcode/arrays/MajorityElement.java
@@ -0,0 +1,24 @@
+package leetcode.arrays;
+
+import java.util.HashMap;
+
+/**
+ * Created by shanwu on 16-12-29.
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap();
+        int max =0;
+        int max_value = 0;
+        for(int i =0 ; i < nums.length; i++) {
+            int count = (map.get(nums[i]) == null) ? 1 : map.get(nums[i]) + 1;
+            if(count > max) {
+                max = count;
+                max_value = nums[i];
+            }
+            map.put(nums[i],count);
+        }
+
+        return max_value;
+    }
+}
