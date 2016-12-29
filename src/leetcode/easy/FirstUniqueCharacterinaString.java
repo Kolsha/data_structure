1ec81b67e92be5650be121989bc33a0daf386909
shanwu <shanwustore@gmail.com>
2016-12-29 17:59:01 +0800

    change: add leetcode practice~ first unique char in a string

---
+++ src/leetcode/easy/FirstUniqueCharacterinaString.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by shanwu on 16-12-29.
+ */
+public class FirstUniqueCharacterinaString {
+    public static int firstUniqChar(String s) {
+        HashMap<Integer, Integer> map = new HashMap();
+        ArrayList<Integer> list = new ArrayList();
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = (int) s.charAt(i);
+
+            if (map.get(c) == null) {
+                map.put(c, i);
+                list.add(new Integer(i));
+            } else {
+                int index = map.get(c);
+                list.remove(new Integer(index));
+            }
+
+        }
+        return list.isEmpty() ? -1 : (int) list.get(0);
+    }
+}
