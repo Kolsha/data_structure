3963608ee847da5b3a1a327f72a475410a8ba1e8
shanwu <shanwustore@gmail.com>
2017-01-01 23:27:24 +0800

    change: add leetcode practice~ number of 1 bits

--- src/exam/review/leetcode/bit_operation/NumberOf1Bits.java
+++ src/exam/review/leetcode/bit_operation/NumberOf1Bits.java
@@ -6,6 +6,7 @@ package exam.review.leetcode.bit_operation;
 public class NumberOf1Bits {
     // you need to treat n as an unsigned value
     // http://docs.oracle.com/javase/tutorial/java/nutsandbolts/op3.html
+    // http://stackoverflow.com/questions/2811319/difference-between-and
     public int hammingWeight(int n) {
         int count = 0;
         while(n!=0) {
