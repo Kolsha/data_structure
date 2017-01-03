ef06e1dee2764f7132565938918ae7b54152d6d0
shanwu <shanwustore@gmail.com>
2017-01-04 00:46:09 +0800

    change: add leetcode practice ~ swap nodes in pairs

---
+++ src/exam/review/leetcode/singly_linked_list/SwapNodesInPairs.java
@@ -0,0 +1,31 @@
+package exam.review.leetcode.singly_linked_list;
+
+/**
+ * Created by shanwu on 17-1-4.
+ */
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode result = head;
+        boolean isSwap = true;
+        while (result != null) {
+            if (isSwap && result.next != null) {
+                // swap value only
+                int temp = result.val;
+                result.val = result.next.val;
+                result.next.val = temp;
+            }
+            isSwap = !isSwap;
+            result = result.next;
+        }
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
