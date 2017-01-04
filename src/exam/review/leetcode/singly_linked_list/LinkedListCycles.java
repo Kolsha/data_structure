df1d4f7f37365833da04e761be793f59e85bbec3
shanwu <shanwustore@gmail.com>
2017-01-04 13:57:26 +0800

    change: add leetcode practice ~linked list cycle

---
+++ src/exam/review/leetcode/singly_linked_list/LinkedListCycles.java
@@ -0,0 +1,35 @@
+package exam.review.leetcode.singly_linked_list;
+
+/**
+ * Created by shanwu on 17-1-4.
+ */
+public class LinkedListCycles {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode walker = head;
+        ListNode runner = head;
+        while (runner.next != null && runner.next.next != null) {
+
+            runner = runner.next.next;
+            walker = walker.next;
+            if (walker == runner) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
