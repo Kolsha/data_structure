adb08bc133f8c111d66b919bbbcba645b80a32a1
shanwu <shanwustore@gmail.com>
2017-01-05 18:28:54 +0800

    change: refactor class name

--- src/exam/review/think_in_java/inner_class/InnerTest.java
+++ src/exam/review/think_in_java/inner_class/InnerTest.java
@@ -3,24 +3,21 @@ package exam.review.think_in_java.inner_class;
 /**
  * Created by shanwu on 17-1-3.
  */
-public class InterviewQuestion {
+public class InnerTest {
     private String mContent = "hi";
 
     class B {
         private String mContent = "hi, B";
 
         class C {
-            private String mContent = "hi, C";
-
             public void test() {
-                System.out.println(InterviewQuestion.this.mContent);
+                System.out.println(InnerTest.this.mContent);
+                System.out.println(B.this.mContent);
             }
         }
     }
 
     public static void main(String[] args) {
-        InterviewQuestion.B.C c = new InterviewQuestion().new B().new C();
-        c.test();
-
+        new InnerTest().new B().new C().test();
     }
 }
