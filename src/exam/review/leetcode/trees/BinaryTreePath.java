edbda0c885a4bcf683bd9685576fc1aa93a244e3
shanwu <shanwustore@gmail.com>
2017-01-14 01:02:33 +0800

    change: add leetcode practice ~ binary tree path

---
+++ src/exam/review/leetcode/trees/BinaryTreePath.java
@@ -0,0 +1,37 @@
+package exam.review.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shanwu on 17-1-14.
+ */
+public class BinaryTreePath {
+    public List<String> binaryTreePaths(TreeNode root) {
+        ArrayList<String> path = new ArrayList();
+        if (root != null) binaryTreePaths(root, "", path);
+        return path;
+    }
+
+    private void binaryTreePaths(TreeNode root, String path, List record) {
+        if (root.left == null && root.right == null) {
+            record.add(path + root.val);
+            return;
+        }
+
+        if (root.left != null) binaryTreePaths(root.left, path + root.val + "->", record);
+
+        if (root.right != null) binaryTreePaths(root.right, path + root.val + "->", record);
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
