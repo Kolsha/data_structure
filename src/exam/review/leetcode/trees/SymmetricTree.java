ed823c5f93b14ccee61a9516fd93b98e5c149b53
shanwu <shanwustore@gmail.com>
2017-01-13 22:51:51 +0800

    change: add leetcode practice ~ symmetric tree

---
+++ src/exam/review/leetcode/trees/SymmetricTree.java
@@ -0,0 +1,33 @@
+package exam.review.leetcode.trees;
+
+/**
+ * Created by shanwu on 17-1-13.
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
