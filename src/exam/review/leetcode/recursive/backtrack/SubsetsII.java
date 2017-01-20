4cfc22b42b1a01cdb8134c7a94a40177863a1b87
shanwu <shanwustore@gmail.com>
2017-01-20 18:36:35 +0800

    change: add leetcode practice ~ subset II

---
+++ src/exam/review/leetcode/recursive/backtrack/SubsetsII.java
@@ -0,0 +1,28 @@
+package exam.review.leetcode.recursive.backtrack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by shanwu on 17-1-20.
+ */
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        backtrack(result, new ArrayList(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+        result.add(new ArrayList(temp));
+        final int size = nums.length;
+        for(int i = start; i < size; i++) {
+            if(i > start && nums[i] == nums[i-1]) continue;
+            temp.add(nums[i]);
+            backtrack(result, temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
