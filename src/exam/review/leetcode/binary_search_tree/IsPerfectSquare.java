a3dd959d5c331eb349764bac69e57b16231abf77
shanwu <shanwustore@gmail.com>
2017-01-23 23:57:30 +0800

    change: add leetcode practice ~ is perfect square

---
+++ src/exam/review/leetcode/binary_search_tree/IsPerfectSquare.java
@@ -0,0 +1,22 @@
+package exam.review.leetcode.binary_search_tree;
+
+/**
+ * Created by shanwu on 17-1-23.
+ */
+public class IsPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        long left = 1, right = num;
+        while(left <= right) {
+            long mid = left+(right-left)/2;
+            long t = mid * mid;
+            if(t > num) {
+                right = mid -1;
+            } else if(t < num){
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
