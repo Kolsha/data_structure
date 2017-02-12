c56bf8be6d360bc2fc81ec070a6f0514c5071dee
shanwu <sungzheguang@hotmail.com>
2017-02-12 15:41:25 +0800

    change: add leetcode practice ~ unique path

---
+++ src/exam/review/leetcode/dynamic_programming/UniquePaths.java
@@ -0,0 +1,33 @@
+package exam.review.leetcode.dynamic_programming;
+
+import java.util.HashMap;
+public class UniquePaths {
+    /**
+     *  Dynamic Programming approach.
+     *  https://www.topcoder.com/community/data-science/data-science-tutorials/dynamic-programming-from-novice-to-advanced/
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        Integer[][] map = new Integer[m][n];
+
+        for(int i = 0; i < m; i++) {
+            map[i][0] = 1;
+        }
+
+        for(int j = 0; j< n; j++) {
+            map[0][j] = 1;
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                map[i][j] = map[i-1][j] + map[i][j-1];
+            }
+        }
+
+        return map[m-1][n-1];
+
+    }
+
+}
