3a6a7bafc1abe25915cb0ae74893044e8f3e743a
shanwu <sungzheguang@hotmail.com>
2017-02-13 14:22:19 +0800

    change: add leetcode practice ~ valid perfect square

--- src/exam/review/leetcode/binary_search_tree/PerfectSquare.java
+++ src/exam/review/leetcode/binary_search_tree/PerfectSquare.java
@@ -2,16 +2,17 @@ package exam.review.leetcode.binary_search_tree;
 
 /**
  * Created by shanwu on 17-1-23.
+ * PC: 1
  */
-public class IsPerfectSquare {
-    public boolean isPerfectSquare(int num) {
+public class PerfectSquare {
+    public boolean ValidPerfectSquare(int num) {
         long left = 1, right = num;
-        while(left <= right) {
-            long mid = left+(right-left)/2;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
             long t = mid * mid;
-            if(t > num) {
-                right = mid -1;
-            } else if(t < num){
+            if (t > num) {
+                right = mid - 1;
+            } else if (t < num) {
                 left = mid + 1;
             } else {
                 return true;
