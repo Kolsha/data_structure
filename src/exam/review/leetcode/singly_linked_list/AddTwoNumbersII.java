3a6b6ad978400605e5ac09a3f548dbd9fb3589f6
shanwu <sungzheguang@hotmail.com>
2017-02-15 13:49:51 +0800

    change: add leetcode practice ~ add two numbersII

--- src/exam/review/leetcode/singly_linked_list/AddTwoNumbersII.java
+++ src/exam/review/leetcode/singly_linked_list/AddTwoNumbersII.java
@@ -1,7 +1,10 @@
 package exam.review.leetcode.singly_linked_list;
 
+import java.util.Stack;
+
 /**
  * Created by shanwu on 17-1-18.
+ * PC: 1
  */
 public class AddTwoNumbersII {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -51,6 +54,48 @@ public class AddTwoNumbersII {
             ListNode newNode = new ListNode(temp);
             newNode.next = result;
             result = newNode;
+        }
+
+        return result;
+
+    }
+
+    public ListNode addTwoNumbersII(ListNode l1, ListNode l2) {
+        Stack<Integer> valStack1 = new Stack<>();
+        Stack<Integer> valStack2 = new Stack<>();
+
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        while (temp1 != null) {
+            valStack1.push(temp1.val);
+            temp1 = temp1.next;
+        }
+
+        while (temp2 != null) {
+            valStack2.push(temp2.val);
+            temp2 = temp2.next;
+        }
+
+        ListNode result = null;
+        int sum = 0;
+        int carry = 0;
+        while (!valStack1.isEmpty() || !valStack2.isEmpty() || carry != 0) {
+            int t1 = (!valStack1.isEmpty()) ? valStack1.pop() : 0;
+            int t2 = (!valStack2.isEmpty()) ? valStack2.pop() : 0;
+            sum += carry + t1 + t2;
+            carry = 0;
+
+            if (sum > 9) {
+                sum = sum - 10;
+                carry = 1;
+            }
+
+            ListNode newNode = new ListNode(sum);
+            newNode.next = result;
+            result = newNode;
+
+            sum = 0;
+
         }
 
         return result;
