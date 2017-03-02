44f203f88cd34e07da4425eb6360609606f7ebfa
shanwu <sungzheguang@hotmail.com>
2017-03-02 19:06:31 +0800

    change: add leetcode practice ~ lowest common ancestor of a binary search tree

---
+++ src/exam/review/leetcode/binary_search_tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,26 @@
+package exam.review.leetcode.binary_search_tree;
+
+/**
+ * Created by shanwu on 17-3-2.
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
