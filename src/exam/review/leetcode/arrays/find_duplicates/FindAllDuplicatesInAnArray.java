6f3f3b15074d82f449c6881a97afc370bd3b18cb
shanwu <sungzheguang@hotmail.com>
2017-03-10 10:52:37 +0800

    change: add leetcode practice ~ find duplicates

--- src/exam/review/leetcode/arrays/find_duplicates/FindAllDuplicatesInAnArray.java
+++ src/exam/review/leetcode/arrays/find_duplicates/FindAllDuplicatesInAnArray.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 /**
  * Created by shanwu on 17-1-15.
+ * PC: 1
  */
 public class FindAllDuplicatesInAnArray {
     public List<Integer> findDuplicates(int[] nums) {
@@ -30,5 +31,32 @@ public class FindAllDuplicatesInAnArray {
 
         return result;
 
+    }
+
+    /**
+     * According to provided condition, we have an array of numbers, 1 <= value of number <= sizeOfArray
+     * from the condition, we can use the value of the number as index in the array, negate the value it indicates,
+     * therefore when we iterate through the array, once we find the negative value, we will know it's duplicated number
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDuplicatesII(int[] nums) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) {
+            return result;
+        }
+        final int size = nums.length;
+        for(int i = 0; i < size; i++) {
+            final int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0) {
+                // negate the value to mark
+                nums[index] = - nums[index];
+            } else {
+                // if nums[index] is negative means we already have the value
+                result.add(Math.abs(nums[i]));
+            }
+        }
+
+        return result;
     }
 }
---
+++ src/exam/review/leetcode/dynamic_programming/DecodeWays.java
@@ -0,0 +1,9 @@
+package exam.review.leetcode.dynamic_programming;
+
+/**
+ * @see <a href="http://stackoverflow.com/questions/20342462/review-an-answer-decode-ways">Detailed explaination </a>
+ */
+public class DecodeWays {
+    // TODO: 17-3-6
+
+}
