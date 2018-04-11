6255d38396dc007df831229e37f505acef00e10e
shanwu <sungzheguang@hotmail.com>
2018-04-11 22:33:22 +0800

    Review: Path sum

--- src/exam/review/leetcode/trees/iterater_path/PathSum.java
+++ src/exam/review/leetcode/trees/iterater_path/PathSum.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by shanwu on 17-1-14.
+ * Question description:
+ * https://leetcode.com/problems/path-sum/description/
  * PC: 1
  */
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
         ArrayList<Integer> pathSumList = new ArrayList();
 
-        if(root!=null) hasPathSum(root, 0, pathSumList);
+        if (root != null) hasPathSum(root, 0, pathSumList);
 
-        for(Integer num:pathSumList) {
-            if(num == sum) {
+        for (Integer num : pathSumList) {
+            if (num == sum) {
                 return true;
             }
         }
@@ -22,40 +23,41 @@ public class PathSum {
     }
 
     private void hasPathSum(TreeNode root, int sum, List record) {
-        if(root.left == null && root.right == null) {
-            record.add(sum+root.val);
+        if (root.left == null && root.right == null) {
+            record.add(sum + root.val);
         }
 
-        if(root.left!=null) hasPathSum(root.left, sum+root.val, record);
-        if(root.right!=null) hasPathSum(root.right, sum+root.val,record);
+        if (root.left != null) hasPathSum(root.left, sum + root.val, record);
+        if (root.right != null) hasPathSum(root.right, sum + root.val, record);
     }
 
     /**
-     *  my 2nd approach
+     * my 2nd approach
+     *
      * @param root
      * @param sum
      * @return
      */
     public boolean hasPathSumII(TreeNode root, int sum) {
-        return findPathSum(root,sum,0);
+        return findPathSum(root, sum, 0);
     }
 
     private boolean findPathSum(TreeNode root, int sum, int curr) {
-        if(root == null) {
+        if (root == null) {
             return false;
         }
 
-        if(isExternal(root)) {
-            return (curr+root.val) == sum;
+        if (isExternal(root)) {
+            return (curr + root.val) == sum;
         }
 
         boolean findLeft = false, findRight = false;
-        if(root.left!=null) {
-            findLeft = findPathSum(root.left, sum, curr+root.val);
+        if (root.left != null) {
+            findLeft = findPathSum(root.left, sum, curr + root.val);
         }
 
-        if(root.right!=null) {
-            findRight = findPathSum(root.right, sum, curr+root.val);
+        if (root.right != null) {
+            findRight = findPathSum(root.right, sum, curr + root.val);
         }
 
         return findLeft || findRight;
@@ -63,6 +65,35 @@ public class PathSum {
 
     private boolean isExternal(TreeNode root) {
         return root.left == null && root.right == null;
+    }
+
+    /**
+     * My 3rd approach
+     *
+     * @param root
+     * @param sum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return (root == null) ? false : hasSum(root, 0, sum);
+    }
+
+    private boolean hasSum(TreeNode root, int sum, int target) {
+        if (root.left == null && root.right == null) {
+            sum += root.val;
+            return (sum == target);
+        }
+
+        boolean leftResult = false, rightResult = false;
+        if (root.left != null) {
+            leftResult = hasSum(root.left, sum + root.val, target);
+        }
+
+        if (root.right != null) {
+            rightResult = hasSum(root.right, sum + root.val, target);
+        }
+
+        return leftResult || rightResult;
     }
 
     public class TreeNode {
