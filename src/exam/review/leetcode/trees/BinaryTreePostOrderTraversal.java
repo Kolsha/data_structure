8a451f85f43e7e9f956163076a7fe41ab7575619
shanwu <sungzheguang@hotmail.com>
2018-04-13 06:36:30 +0800

    Review: Binary Tree Postorder Traversal

--- src/exam/review/leetcode/trees/BinaryTreePostOrderTraversal.java
+++ src/exam/review/leetcode/trees/BinaryTreePostOrderTraversal.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by shanwu on 16-12-18.
- * PC: 1
+ * Question description:
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ * PC: 2
  */
 public class BinaryTreePostOrderTraversal {
     public List<Integer> postorderTraversal(BinaryTreePreorderTraversal.TreeNode root) {
-        List<Integer> list = new ArrayList();
-        if(root == null) {
-            return list;
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
         }
 
-        if(root.left!=null)
-            list.addAll(postorderTraversal(root.left));
-        if(root.right!=null)
-            list.addAll(postorderTraversal(root.right));
+        if (root.left != null) {
+            result.addAll(postorderTraversal(root.left));
+        }
 
-        list.add(root.val);
-        return list;
+        if (root.right != null) {
+            result.addAll(postorderTraversal(root.right));
+        }
+
+        result.add(root.val);
+
+        return result;
     }
 
     // TODO: 16-12-18 Note: Recursive solution is trivial, could you do it iteratively?
