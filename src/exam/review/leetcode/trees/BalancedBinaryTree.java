7ab54674516d2ed54b682066d6fe17c34cbbfab9
shanwu <sungzheguang@hotmail.com>
2018-04-16 06:54:55 +0800

    Review: Balanced binary tree

--- src/exam/review/leetcode/trees/BalancedBinaryTree.java
+++ src/exam/review/leetcode/trees/BalancedBinaryTree.java
@@ -1,28 +1,25 @@
 package exam.review.leetcode.trees;
 
 /**
- * Created by shanwu on 16-12-18.
+ * Question description:
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ * PC: 2
  */
 public class BalancedBinaryTree {
-    /**
-     * the easiest solution
-     * PC: 1
-     * @param root
-     * @return
-     */
-    public boolean isBalanced(BinaryTreePreorderTraversal.TreeNode root) {
+    public boolean isBalanced(TreeNode root) {
         if (root == null) {
             return true;
         }
-        return Math.abs(height(root.left) - height(root.right)) <= 1 &&
-                isBalanced(root.left) &&
-                isBalanced(root.right);
+
+        return Math.abs(getTreeHeight(root.left) - getTreeHeight(root.right)) <= 1 &&
+                isBalanced(root.left) && isBalanced(root.right);
     }
 
-    public int height(BinaryTreePreorderTraversal.TreeNode root) {
+    private int getTreeHeight(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        return Math.max(height(root.left), height(root.right)) + 1;
+
+        return Math.max(getTreeHeight(root.left), getTreeHeight(root.right)) + 1;
     }
 }
